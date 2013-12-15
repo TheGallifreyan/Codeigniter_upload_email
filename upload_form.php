6bbe942cccedc091ac58599c58b30e65dedd3645
TheGallifreyan <caseyh1900@gmail.com>
2013-12-15 09:32:48 -0500

    PHP code which creates the form

---
+++ upload_form.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html>
+<head>
+<title>Upload Form</title>
+</head>
+<body>
+<?php echo $error;?>
+<?php echo form_open_multipart('upload/do_upload');?>
+	<input type="file" name="userfile" size="20" />
+		<br><br>
+	<input type="text" name="name" size="20" />
+		<br><br>
+	<input type="email" name="email" size="20" />
+	<input type="submit" value="upload" />
+</form>
+</body>
+</html>
